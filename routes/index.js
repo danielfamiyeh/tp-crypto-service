ae9e80d49e904a042b35e66ed6ced58c8d09da8c
danielfamiyeh <58159259+danielfamiyeh@users.noreply.github.com>
2021-03-05 09:38:39 +0000

    This is basically everything.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const router = require("express").Router();
-const { getSearchResults } = require("./../controllers");
+const {
+	getSearchResults,
+	getInfo,
+	getTicks,
+	getPrice,
+	getDepth,
+} = require("./../controllers");
 
 router.get("/", (req, res) =>
 	res.json({ msg: "Greetings from the Crypto service." })
 );
 
+router.get("/:symbol/info", getInfo);
+router.get("/:symbol/depth", getDepth);
+router.get("/:symbol/price", getPrice);
 router.get("/search/:query", getSearchResults);
+router.get("/:symbol/ticks/:interval/:from/:to", getTicks);
 
 module.exports = router;
